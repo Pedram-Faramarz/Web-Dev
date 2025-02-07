87c86bc492a537ece4d21043d3ad5019124776ed
Pedram-Faramarz <pedramfaramarz2023@gmail.com>
2025-02-07 15:06:13 +0500

    end of task one

---
+++ Lab3/Task1/rewrite if_else_1.js
@@ -0,0 +1,16 @@
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
