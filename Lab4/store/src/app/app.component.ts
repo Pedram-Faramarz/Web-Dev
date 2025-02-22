804b39e0fed42f2cf376c32013dd8c2131db271f
Pedram-Faramarz <pedramfaramarz2023@gmail.com>
2025-02-23 03:52:03 +0500

    New push

--- Lab4/store/src/app/app.component.ts
+++ Lab4/store/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ProductsComponent } from "./products/products.component";
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductsComponent],
+  imports: [ProductsComponent, TestComponent],
   //templateUrl: './app.component.html',
-  template:`<app-products></app-products>`,
+  template:`<app-products></app-products>
+            <div><app-test></app-test></div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
